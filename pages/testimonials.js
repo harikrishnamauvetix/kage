a2c3c4e7320d3a9fb1dbb76873a3fb3907d381ba
mounika <harikrishna@mauvetix.com>
2024-12-30 11:28:37 +0530

    ss

--- pages/testimonials.js
+++ pages/testimonials.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Typography, Grid } from '@mui/material';
-import TestimonialCard from '../components/TestimonialCard';
+
 
 const Testimonials = () => {
   const testimonials = [
